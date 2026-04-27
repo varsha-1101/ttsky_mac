86be882952374c86a6f3dbc67b99f8bac97134e9
varsha-1101 <varsharamesh1101@gmail.com>
2026-04-27 09:38:17 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -9,40 +6,40 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
 
-    # -----------------------------
-    # Start clock (IMPORTANT)
-    # -----------------------------
+    # Start clock
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # -----------------------------
-    # Reset
-    # -----------------------------
+    # Initialize inputs
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
 
-    await ClockCycles(dut.clk, 5)
+    # Apply reset
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 2)
     dut.rst_n.value = 1
+
+    # Wait 1 cycle for reset to settle
+    await ClockCycles(dut.clk, 1)
 
     # -----------------------------
     # Cycle 1: 2 * 3 = 6
     # -----------------------------
     dut.ui_in.value = (3 << 4) | 2
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
     assert int(dut.uo_out.value) == 6
 
     # -----------------------------
-    # Cycle 2: + (1 * 4) = 10
+    # Cycle 2: 6 + (1 * 4) = 10
     # -----------------------------
     dut.ui_in.value = (4 << 4) | 1
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
     assert int(dut.uo_out.value) == 10
 
     # -----------------------------
-    # Cycle 3: + (2 * 2) = 14
+    # Cycle 3: 10 + (2 * 2) = 14
     # -----------------------------
     dut.ui_in.value = (2 << 4) | 2
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 2)
     assert int(dut.uo_out.value) == 14
